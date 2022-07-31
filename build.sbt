39f4d2056bde986c74e5cc920baa758837ac9cc6
Denys <denys.grushchak@studio.unibo.it>
2022-07-31 15:48:03 +0200

    minor fixes

--- build.sbt
+++ build.sbt
@@ -1,6 +1,7 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.1.3"
+//ThisBuild / scalaVersion := "3.2.0-RC3" // for coverage testing
 
 val scalatest = "org.scalatest" %% "scalatest-funsuite" % "3.2.12" % Test
 
---
+++ src/main/scala/todo/MyClass.scala
@@ -0,0 +1,29 @@
+package todo
+
+class MyClass:
+  var y: Int = 999
+  def getValue: Int = y
+
+  def PrintValue(x: Int): Unit =
+    (0 to x by 1).foreach(i =>
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+    );
+
+  def PrintValue2(x: Int): Unit =
+    (0 to x by 1).foreach(i =>
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+      print(s"$i + Calambur")
+    );
