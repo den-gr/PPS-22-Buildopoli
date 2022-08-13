0a6e0d031fe06a4b2f437236949421dbe3854204
Denys <denys.grushchak@studio.unibo.it>
2022-08-13 10:57:19 +0200

    refactoring behaviour

--- src/main/scala/behaviour/BehaviourModule.scala
+++ src/main/scala/behaviour/BehaviourModule.scala
@@ -11,29 +11,36 @@ object BehaviourModule extends App:
   type Choose = (Int, Int)
 
   trait Behaviour:
-    def currentStories: Seq[StoryGroup]
+    def startBehaviour: Seq[StoryGroup]
 
     /** Allow to choose an available event of this behaviour
       * @param choose
       *   see [[Choose]]
       */
-    def chooseEvent(playerId: Int, choose: Choose): Unit
+    def chooseEvent(playerId: Int, choose: Choose): Seq[StoryGroup]
 
   object Behaviour:
     def apply(initialEvents: Seq[EventGroup]): Behaviour = BehaviourImpl(initialEvents)
 
     case class BehaviourImpl(initialEvents: Seq[EventGroup]) extends Behaviour:
       var currentEvents: Seq[EventGroup] = Seq()
-      override def currentStories: Seq[StoryGroup] =
-        currentEvents = initialEvents;
-        println("Run Behaviour")
-        println(s"> ${initialEvents.head.head.eventStory.description}")
-        println(s">index  ${initialEvents.indexOf(initialEvents.head)}")
-        initialEvents.map(eventGroup => eventGroup.map(m => m.eventStory))
+      override def startBehaviour: Seq[StoryGroup] =
+        currentEvents = initialEvents
+        getStories
 
-      override def chooseEvent(playerId: Int, choose: Choose): Unit =
+      override def chooseEvent(playerId: Int, choose: Choose): Seq[StoryGroup] =
         try
           val nextOpEvent = currentEvents(choose._1)(choose._2).run(playerId)
+          println(s"Before $currentEvents")
+
+          // remove used EventGroup
           currentEvents = currentEvents.patch(choose._1, Nil, 1)
-//          if nextOpEvent.nonEmpty then nextOpEvent.get
-        catch case e: IndexOutOfBoundsException => throw IllegalArgumentException("Player chose not existing event")
+          println(s"After $currentEvents")
+
+          // insert new event Group
+          if nextOpEvent.nonEmpty then currentEvents = currentEvents :+ Seq(nextOpEvent.get)
+        catch case _: IndexOutOfBoundsException => throw IllegalArgumentException("Player chose not existing event")
+        getStories
+
+      private def getStories: Seq[StoryGroup] =
+        currentEvents.map(eventGroup => eventGroup.map(m => m.eventStory))
