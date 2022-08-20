7d16833a5648309f044e2179994e198696690cfd
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2022-08-20 14:29:32 +0200

    added new rent strategy implementation and test

--- src/main/scala/terrain/Rent.scala
+++ src/main/scala/terrain/Rent.scala
@@ -1,9 +1,11 @@
 package terrain
 
+import Buildable.Token
 object Rent :
 
   trait KnowsTerrains:
     def getNTerrain: Int
+    def isGroupComplete: Boolean
 
   trait RentStrategy:
     def computeTotalRent: Int
@@ -11,8 +13,13 @@ object Rent :
   case class RentStrategyWithBonus(startingPrice: Int, bonus:Int, kt: KnowsTerrains) extends RentStrategy:
     override def computeTotalRent: Int = startingPrice + bonus * (kt.getNTerrain - 1)
 
-  case class RentStrategyMultiplier(startingPrice:Int, multiplier: Int, kt: KnowsTerrains) extends RentStrategy:
+  case class RentStrategyPreviousPriceMultiplier(startingPrice: Int, multiplier: Int, kt: KnowsTerrains) extends RentStrategy:
     def recursion(nTerrains: Int): Int = nTerrains match
       case 1 => startingPrice
       case n => multiplier * recursion(nTerrains-1)
     override def computeTotalRent: Int = recursion(kt.getNTerrain)
+
+  case class BasicRentStrategyFactor(startingPrice: Int, factor: Int, kt: KnowsTerrains) extends RentStrategy:
+    override def computeTotalRent: Int = kt.isGroupComplete match
+      case true => startingPrice * factor
+      case false => startingPrice
