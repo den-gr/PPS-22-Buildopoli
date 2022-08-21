91ce25423c7d517deab37cf942814a2e05217902
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2022-08-21 20:45:56 +0200

    added documentation for purchasable

--- src/main/scala/terrain/Purchasable.scala
+++ src/main/scala/terrain/Purchasable.scala
@@ -6,21 +6,70 @@ import Rent.*
 
 object Purchasable :
 
+  /**
+   * This enum represent the state of a purchasable terrain
+   * IN_BANK if the terrain has not been purchased by anyone
+   * OWNED if a owner possess the terrain and it is active
+   * MORTGAGED if a owner possess the terrain but they have temporarily sold it to the bank so it is not active
+   */
   enum PurchasableState:
     case IN_BANK, OWNED, MORTGAGED
 
-  //The properties of a Purchasable terrain
+  /**
+   * It represents the properties of a terrain that can be purchased by a player
+   */
   trait Purchasable extends Terrain:
+    /**
+     * @return the price at which the player can buy the terrain
+     */
     def price: Int
+    /**
+     *
+     * @return the group to which the terrain belongs
+     */
     def group: String
+    /**
+     *
+     * @return the state the terrain is currently in
+     */
     def state: PurchasableState
+    /**
+     *
+     * @return the owner of the terrain
+     */
     def owner: Option[Int]
-
+    /**
+     * It is used to change the ownership of the terrain
+     * @param newOwner the new owner of the terrain
+     * @return a new Purchasable object
+     */
     def changeOwner(newOwner: Option[Int]): Purchasable
+    /**
+     *
+     * @return the total rent a player owes to the owner of the terrain
+     */
     def computeTotalRent: Int
+    /**
+     * It is used to mortgage the purchasable terrain to the bank
+     * @return a new Purchasable object
+     */
     def mortgage: Purchasable
+    /**
+     *
+     * @return the total amount of money the owner gets if mortgage the purchasable terrain
+     */
     def computeMortgage: Int
 
+  /**
+   * The standard implementation of a purchasable terrain
+   * @param terrain the basic terrain
+   * @param price the price at which the terrain can be bought
+   * @param group the group to which the terrain belongs
+   * @param ms the strategy to compute the mortgage price
+   * @param rs the strategy to compute the rent
+   * @param owner the owner of the terrain
+   * @param state the state of the terrain in game
+   */
   case class PurchasableTerrain(private val terrain: Terrain, override val price: Int,
                                 override val group: String, ms: MortgageStrategy,
                                 rs: RentStrategy, override val owner:Option[Int] = Option.empty,
