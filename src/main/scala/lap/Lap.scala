8aa3281eae110b2641aeb71338c4ead7962ed655
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2022-08-22 20:00:40 +0200

    fixed lap documentation

--- src/main/scala/lap/Lap.scala
+++ src/main/scala/lap/Lap.scala
@@ -9,6 +9,10 @@ object Lap :
    * It represent the reward given to the player that has completed a new lap
    */
   trait Reward:
+    /**
+     * It is the action performed to give the reward to the player
+     * @param playerID the ID of the player that receives the reward
+     */
     def triggerBonus(playerID: Int): Unit
 
   case class MoneyReward(bank: Bank, money: Int) extends Reward:
@@ -18,12 +22,15 @@ object Lap :
    * Buildopoli's terrains are displayed in circle, each player can complete a lap and gain a reward
   */
   trait Lap:
+
     /**
-     * Says if the checked player has started a new lap
-     * @param isValid says if the movement is valid to gain the reward
-     * @param playerID checked player's ID
-     * @param newPosition the new player's position
-     * @return a Boolean value that says if the player has started a new lap
+     * Says if the checked player has started a new lap and returns also the new player's position
+     * @param isValid says if the movement is valid to take the reward. If it is explicitly said
+     *                that it can not receive the reward or if the movement is backwards it is not valid
+     * @param playerCurrentPosition the current position of the player
+     * @param nSteps the number of additional steps the player has to take
+     * @param nCells the number of cells in the game board
+     * @return the new position and a value that says if a new lap has started or not
      */
     def isNewLap(isValid: Boolean, playerCurrentPosition: Int, nSteps: Int, nCells: Int): (Int, Boolean)
 
@@ -36,7 +43,6 @@ object Lap :
 
   /**
    * A basic implementation of lap
-   * @param playerPosition the Seq of player's positions
    */
   case class GameLap() extends Lap:
     override def isNewLap(isValid: Boolean, playerCurrentPosition: Int, nSteps: Int, nCells: Int): (Int, Boolean) = playerCurrentPosition + nSteps > nCells match
