eb2499ecaf5241c882b037b70f5a9e9a4369d0d6
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2022-08-24 17:53:14 +0200

    refactoring of Rent and implementation to work with group manager, fix of the documentation

--- src/main/scala/terrain/Rent.scala
+++ src/main/scala/terrain/Rent.scala
@@ -1,49 +1,50 @@
 package terrain
 
+import terrain.GroupManager.*
 object Rent :
-  /**
-   * It represents the entity that knows the information related to the terrain's group
-   */
-  trait KnowsTerrains:
-    def getNTerrain: Int
-    def isGroupComplete: Boolean
 
   /**
    * It represents the strategy to calculate the rent that a player owes to the terrain's owner if
    * there are no token
    */
   trait RentStrategy:
-    def computeTotalRent: Int
+    /**
+     * It represents the strategy to calculate the rent that a player owes to the terrain's owner if
+     * there are no token
+     * @param ownerID the owner's ID
+     * @param group the terrain's group
+     * @param groupManager the entity that provides information about the group
+     * @return the price of the rent
+     */
+    def computeTotalRent(ownerID: Int, group: String, groupManager: GroupManager): Int
 
   /**
    * A rent strategy that adds to the starting price a bonus for every additional terrain in the group
    * @param startingPrice the basic price
    * @param bonus the amount of additional bonus
-   * @param kt the entity that provide the information of the terrain's group
    */
-  case class RentStrategyWithBonus(startingPrice: Int, bonus:Int, kt: KnowsTerrains) extends RentStrategy:
-    override def computeTotalRent: Int = startingPrice + bonus * (kt.getNTerrain - 1)
+  case class RentStrategyWithBonus(startingPrice: Int, bonus:Int) extends RentStrategy:
+    override def computeTotalRent(ownerID: Int, group: String, gm: GroupManager): Int =
+      startingPrice + bonus * (gm.sameGroupTerrainsOwned(ownerID, group) - 1)
 
   /**
    * A rent strategy that for each additional terrain's multiplies the previous rent
    * @param startingPrice the basic price
    * @param multiplier the multiplier of the previous price
-   * @param kt the entity that provide the information of the terrain's group
    */
-  case class RentStrategyPreviousPriceMultiplier(startingPrice: Int, multiplier: Int, kt: KnowsTerrains) extends RentStrategy:
+  case class RentStrategyPreviousPriceMultiplier(startingPrice: Int, multiplier: Int) extends RentStrategy:
     def recursion(nTerrains: Int): Int = nTerrains match
       case 1 => startingPrice
       case n => multiplier * recursion(nTerrains-1)
-    override def computeTotalRent: Int = recursion(kt.getNTerrain)
+    override def computeTotalRent(ownerID: Int, group: String, gm: GroupManager): Int = recursion(gm.sameGroupTerrainsOwned(ownerID, group))
 
   /**
    * A rent strategy that multiplies by a factor the starting price if all the terrains in the group belong to the
    * same owner
    * @param startingPrice the basic price
    * @param factor the factor used to multiply the price
-   * @param kt the entity that provide the information of the terrain's group
    */
-  case class BasicRentStrategyFactor(startingPrice: Int, factor: Int, kt: KnowsTerrains) extends RentStrategy:
-    override def computeTotalRent: Int = kt.isGroupComplete match
+  case class BasicRentStrategyFactor(startingPrice: Int, factor: Int) extends RentStrategy:
+    override def computeTotalRent(ownerID: Int, group: String, gm: GroupManager): Int = gm.isGroupComplete(ownerID, group) match
       case true => startingPrice * factor
       case false => startingPrice
