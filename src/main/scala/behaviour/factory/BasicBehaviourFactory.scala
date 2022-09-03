eb014216fe9b3fb17370766cf7ae17eefbe25083
Denys <denys.grushchak@studio.unibo.it>
2022-09-03 16:56:16 +0200

    refactoring

--- src/main/scala/behaviour/factory/BasicBehaviourFactory.scala
+++ src/main/scala/behaviour/factory/BasicBehaviourFactory.scala
@@ -2,19 +2,17 @@ package behaviour.factory
 
 import behaviour.BehaviourModule.Behaviour
 import behaviour.event.EventModule.Event
-import behaviour.factory.input.JailBehaviourInput
-import behaviour.factory.input.tempate.JailBehaviourDefaultInput
+import behaviour.factory.input.{JailBehaviourDefaultInput, JailBehaviourInput}
 
 /** Allows easily to create basic behaviours
   */
 trait BasicBehaviourFactory:
 
   /** @param input
-    * a box that contains all inputs that are necessary to create s jail behaviour. By default it uses the values of
-    * [[JailBehaviourDefaultInput]] object
+    *   a box that contains all inputs that are necessary to create s jail behaviour. By default it uses the values of
+    *   [[JailBehaviourDefaultInput]] object
     * @return
     *   jail behaviour that imprison a player for [[input#blockingTime]] turns. Include also the possibility to escape
     *   from prison
     */
-  def JailBehaviour(input: JailBehaviourInput = JailBehaviourDefaultInput): Behaviour
-  
+  def JailBehaviour(input: JailBehaviourInput = JailBehaviourDefaultInput()): Behaviour
