d67777ba17d95417ea1b3e035f786948765d3697
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2022-09-07 10:42:54 +0200

    Add GameInputs to maintain a list with all input values for a player in a turn. With list not empty, is impossible to proceed with next turns.

--- src/main/scala/gameManagement/gameStore/GameStore.scala
+++ src/main/scala/gameManagement/gameStore/GameStore.scala
@@ -1,54 +1,55 @@
 package gameManagement.gameStore
 
+import gameManagement.gameStore.gameInputs.GameInputs
 import player.Player
 import terrain.Terrain
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
-/**
- * Represents some form of storage for the game.
- * Contains list of players and list of terrains, plus some useful methods to manage those lists.
- */
+/** Represents some form of storage for the game. Contains list of players and list of terrains, plus some useful
+  * methods to manage those lists.
+  */
 trait GameStore:
-  /**
-   * Immutable lists of players in the game
-   */
-  var playersList: List[Player]
+  /** Immutable lists of players in the game
+    */
+  var playersList: Seq[Player]
 
-  /**
-   * @return Mutable list of terrains in the game
-   */
-  def terrainList: ListBuffer[Terrain]
-
-  /**
-   * Used to assing an incremental id to players being created
-   */
+  /** Used to assing an incremental id to players being created
+    */
   var playerIdsCounter: Int = 0
 
-  /**
-   * @param playerId identifying one player
-   * @return the player object
-   */
+  def userInputs: GameInputs
+
+  /** @return
+    *   Mutable list of terrains in the game
+    */
+  def terrainList: ListBuffer[Terrain]
+
+  /** @param playerId
+    *   identifying one player
+    * @return
+    *   the player object
+    */
   def getPlayer(playerId: Int): Player
 
-  /**
-   *
-   * @param position identifying one terrain
-   * @return the terrain object
-   */
+  /** @param player
+    *   to put into playersList
+    */
+  def addPlayer(player: Player): Unit
+
+  /** @param position
+    *   identifying one terrain
+    * @return
+    *   the terrain object
+    */
   def getTerrain(position: Int): Terrain
 
-  /**
-   * @param terrain to put into the list
-   */
+  /** @param terrain
+    *   to put into the list
+    */
   def putTerrain(terrain: Terrain*): Unit
 
-  /**
-   * @param player to put into playersList
-   */
-  def addPlayer(player: Player): Unit
-  
   def startGame(): Unit
 
 object GameStore:
---
+++ src/test/scala/gameManagement/gameStore/gameInputs/GameInputsTest.scala
@@ -0,0 +1,52 @@
+package gameManagement.gameStore.gameInputs
+
+import gameManagement.gameOptions.GameOptions
+import gameManagement.gameStore.{GameStore, GameStoreImpl}
+import gameManagement.gameTurn.{DefaultGameTurn, GameTurn}
+import org.scalatest.funsuite.AnyFunSuite
+import player.{Player, PlayerImpl}
+
+class GameInputsTest extends AnyFunSuite:
+  val userInput: GameInputs = UserInputs()
+
+  val selector: (Seq[Player], Seq[Int]) => Int =
+    (playerList: Seq[Player], playerWithTurn: Seq[Int]) =>
+      playerList.filter(el => !playerWithTurn.contains(el.playerId)).head.playerId
+
+  val gameStore: GameStore = GameStoreImpl()
+  val gameOptions: GameOptions = GameOptions(200, 2, 10, 6, selector)
+  val gameTurn: GameTurn = DefaultGameTurn(gameOptions, gameStore)
+
+  test("adding one tail element") {
+    userInput.addTailInputEvent("tailElement")
+    assert(!userInput.isListEmpty)
+    assert(userInput.getHeadElement === "tailElement")
+  }
+
+  test("removing the head element, adding one tail element") {
+    userInput.addTailInputEvent("secondTailElement")
+    assert(!userInput.isListEmpty)
+    assert(userInput.getHeadElement === "tailElement")
+    userInput.removeHeadElement()
+    assert(userInput.getHeadElement === "secondTailElement")
+  }
+
+  test("assert gameInput inside gameStore is empty") {
+    assert(gameStore.userInputs.isListEmpty)
+  }
+
+  test("adding two players and testing turns with list not empty") {
+    gameStore.addPlayer(PlayerImpl(1))
+    gameStore.addPlayer(PlayerImpl(2))
+    assert(gameTurn.selectNextPlayer() === 1)
+    assert(gameTurn.playerWithTurn.head === 1)
+    gameStore.userInputs.addTailInputEvent("inputElement")
+    assertThrows[RuntimeException](gameTurn.selectNextPlayer() === 2)
+  }
+
+  test("emptying list should allow to proceed with turns") {
+    gameStore.userInputs.removeHeadElement()
+    assert(gameStore.userInputs.isListEmpty)
+    assert(gameTurn.selectNextPlayer() === 2)
+    assert(gameTurn.playerWithTurn.isEmpty)
+  }
