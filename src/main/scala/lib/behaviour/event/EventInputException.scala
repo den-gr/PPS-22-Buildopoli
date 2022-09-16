204a929a635c755b6ce1dde4a0c2f704bbc97a83
Denys <denys.grushchak@studio.unibo.it>
2022-09-16 19:55:20 +0200

    minor fix

--- src/main/scala/lib/behaviour/event/EventInputException.scala
+++ src/main/scala/lib/behaviour/event/EventInputException.scala
@@ -2,11 +2,10 @@ package lib.behaviour.event
 
 import lib.gameManagement.gameStore.gameInputs.GameInputs
 
-/** Exception that indicate that input defined in [[GameInputs]] not correspond to
-  * an [[Interaction]] or an [[Event]] expectation
- *
-  * @param msg
+/** Indicate that input defined in [[GameInputs]] not correspond to an [[Interaction]] or an [[Event]] expectation
+  *
+  * @param message
   *   exception message
   */
-class EventInputException(msg: String = "The input in game session not correspond to expected input by Event")
-    extends Exception(msg)
+class EventInputException(message: String = "The input in game session not correspond to expected input by Event")
+    extends Exception(message)
