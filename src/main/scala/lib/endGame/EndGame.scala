3152d8a70a823438011fcdb424e364a0f29c0b5d
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2022-09-22 11:02:52 +0200

    fixed error due to wrong position of private methods in endgame

--- src/main/scala/lib/endGame/EndGame.scala
+++ src/main/scala/lib/endGame/EndGame.scala
@@ -33,20 +33,20 @@ object EndGame:
     */
   def apply(gameStore: GameStore) = NoMoneyNoTerrains(gameStore)
 
+  private def terrainPerPlayer(terrains: Seq[Terrain], id: Int): Int =
+    terrains collect onlyPurchasable count (t => t.state == PurchasableState.OWNED && t.owner.get == id)
+
   case class NoMoneyNoTerrains(gameStore: GameStore) extends EndGame:
 
     override def deleteDefeatedPlayer(strategy: Player => Boolean): Unit =
       deleteTerrains(gameStore.playersList filter (p => strategy(p)) map (p => p.playerId))
       gameStore.playersList = gameStore.playersList filter (p => !strategy(p))
 
-  private def terrainPerPlayer(terrains: Seq[Terrain], id: Int): Int =
-    terrains collect onlyPurchasable count (t => t.state == PurchasableState.OWNED && t.owner.get == id)
-
-  private def deleteTerrains(ownerIds: Seq[Int]): Unit =
-    for t <- gameStore.terrainList collect onlyPurchasable filter (x =>
-        x.state != PurchasableState.IN_BANK && (ownerIds contains x.owner.get)
-      )
-    do t.changeOwner(None)
+    private def deleteTerrains(ownerIds: Seq[Int]): Unit =
+      for t <- gameStore.terrainList collect onlyPurchasable filter (x =>
+          x.state != PurchasableState.IN_BANK && (ownerIds contains x.owner.get)
+        )
+      do t.changeOwner(None)
 
   private def onlyPurchasable = new PartialFunction[Terrain, Purchasable]:
     def apply(t: Terrain): Purchasable = t.asInstanceOf[Purchasable]
