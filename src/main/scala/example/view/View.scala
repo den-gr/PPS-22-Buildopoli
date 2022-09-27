37602af5ce094921a0ac6f18b088b5647c98d392
Eleonora Bertoni <eleonora.bertoni3@studio.unibo.it>
2022-09-27 10:32:54 +0200

    added documentation for the View

--- src/main/scala/example/view/View.scala
+++ src/main/scala/example/view/View.scala
@@ -6,30 +6,74 @@ import lib.terrain.Terrain
 import scala.annotation.tailrec
 import scala.io.StdIn.*
 
+/** PlayerChoice represents the choice of the player that can decide to end his turn or to keep going until three single
+  * choices are made
+  */
 enum PlayerChoice:
+  /** It is used when the player decides to end his turn
+    */
   case EndTurn
+
+  /** It is used when the player decides the event group id, the event id and the single action id
+    */
   case Choice(groupIdx: Int, eventIdx: Int, choiceIdx: Int)
 
 trait View:
-
+  /** It determines how the player is displayed
+    * @param playerID
+    *   is the id of the player to be displayed
+    * @param money
+    *   is the current amount of money the player has
+    */
   def showCurrentPlayer(playerID: Int, money: Int): Unit
+
+  /** It determines how the terrain is displayed
+    * @param terrain
+    *   is the cell to be shown
+    * @param position
+    *   is the position of the cell to be shown
+    */
   def showCurrentTerrain(terrain: Terrain, position: Int): Unit
+
+  /** It determines how the available choices are displayed
+    * @param story
+    *   contain the available choices
+    */
   def showStoryOptions(story: Seq[StoryGroup]): Unit
+
+  /** It determines how an error should be displayed
+    * @param msgError
+    *   the explanation of the erro
+    */
   def showError(msgError: String): Unit
+
+  /** It is used to show what the Log object already shows
+    * @param log
+    *   is the Log output
+    */
   def printLog(log: String): Unit
+
+  /** It is used to wait in a blocking way for the player's input
+    * @param story
+    *   represents the available options
+    * @return
+    *   the complete choice
+    */
   def getUserChoices(story: Seq[StoryGroup]): PlayerChoice
 
 case class GameView() extends View:
 
   def END_TURN: Int = -1
-  def showCurrentPlayer(playerID: Int, playerMoney: Int): Unit = println(
+
+  override def showCurrentPlayer(playerID: Int, playerMoney: Int): Unit = println(
     s"\n\n* It is player $playerID turn * \n* Available money: $playerMoney *"
   )
-  def showCurrentTerrain(terrain: Terrain, position: Int): Unit = println(
+
+  override def showCurrentTerrain(terrain: Terrain, position: Int): Unit = println(
     s"* We are currently in >>>${terrain.basicInfo.name}<<< at position number $position *"
   )
 
-  def showStoryOptions(stories: Seq[StoryGroup]): Unit =
+  override def showStoryOptions(stories: Seq[StoryGroup]): Unit =
     var result: String = ""
     stories.zipWithIndex.foreach((storyGroup, i) =>
       result += s"Group Choice $i \n";
@@ -42,19 +86,12 @@ case class GameView() extends View:
 
   override def showError(msgError: String): Unit = println(s"!!! ERROR !!! $msgError")
 
-  def printLog(log: String): Unit = println(s"The log message says ----> $log")
+  override def printLog(log: String): Unit = println(s"The log message says ----> $log")
   def checkInput(s: String): Option[Int] =
     try Some(s.toInt)
     catch case e: Exception => None
 
-  @tailrec
-  private def getChoice(string: String, num: Int): Int =
-    println(string)
-    checkInput(readLine()) match
-      case Some(v) if v < num && v >= END_TURN => v
-      case _ => println("Wrong choice, try again!"); getChoice(string, num)
-
-  def getUserChoices(stories: Seq[StoryGroup]): PlayerChoice =
+  override def getUserChoices(stories: Seq[StoryGroup]): PlayerChoice =
     val gc =
       stories.size match
         case s => getChoice(s"Choose group event or digit $END_TURN to end the turn", s)
@@ -68,3 +105,10 @@ case class GameView() extends View:
   private def forceSingleChoice(size: Int, description: String): Int = size match
     case 1 => println(description); 0
     case s => getChoice("Choose event", s)
+
+  @tailrec
+  private def getChoice(string: String, num: Int): Int =
+    println(string)
+    checkInput(readLine()) match
+      case Some(v) if v < num && v >= END_TURN => v
+      case _ => println("Wrong choice, try again!"); getChoice(string, num)
